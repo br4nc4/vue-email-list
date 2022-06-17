f0440d8d9ea6e7e13d015cb98471ab69bc9fdfe5
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-17 17:03:04 +0200

    completed after ticket DOMENICO

--- script.js
+++ script.js
@@ -1,64 +1,19 @@
 new Vue({
     el:"#app",
     data:{
-        listMail: "",
+        mail: [],
     },
     methods:{
-        test: function(){
-            axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((axiosResp)=>{
-                this.listMail = axiosResp.data.response;
-            })
+        generateMails: function(){
+            for (let i = 0; i < 10; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(resp=>{
+                    this.mail.push(resp.data.response)
+                })
+            }
+            console.log(this.mail);
         }
     }
 });
-
-//TEST COME PRIMO APPROCCIO AD AXIOS
-/* axios
-.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(function(axiosResp){
-    console.log(axiosResp.data.response);
-}) */
-
-/* //TEST PER GENERARE IN UN ARRAY 10 MAIL
-const mailUl = document.querySelector(".mail");
-function check(){
-    if(listMail.length === 10){
-        console.log("limite raggiunto");
-    }
-}
-const listMail = [];
-for(let i=0; i< 10; i++){
-    axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((axiosResp)=>{
-            listMail.push(axiosResp.data.response);
-            check();
-            mailUl.innerText = listMail;
-        })
-}
-console.log(listMail); */
-
-
-
-/* methods:{
-    check: function(){
-        if (this.listMail === 10) {
-            console.log("raggiunto il numero massimo stabilito");
-        }
-    },
-    generateMails: function(){
-        const mailRender = document.getElementById("mail");
-        const url = "https://flynn.boolean.careers/exercises/api/random/mail";
-        for (let i = 0; i < 10; i++) {
-            axios
-                .get(url)
-                .then((axiosResponse)=>{
-                    this.listMail.push(axiosResponse.data.response);
-                    this.check();
-                    mailRender.innerText = this.listMail;
-                })
-        }
-    } */
 
